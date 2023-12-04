03b6d30e0966cb6952c2b736a8970a823d652b11
Mukul <mkpr2000@gmail.com>
2023-12-04 17:15:31 +0530

    "added header and footer components and screens"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from 'react-bootstrap'
+import  Header from './components/Header'
+import Footer from './components/footer'
+import { Outlet } from 'react-router-dom'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+    <>
+    <Header/>
+      <main className='py-3'>
+        <Container>
+          <Outlet/>
+        </Container>
+
+      </main>
+      <Footer/>
+    </>
+  )
 }
 
-export default App;
+export default App
